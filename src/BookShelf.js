a4aca61ead6a90378ca766c34daaeaed257d4400
Rafal Hotlos <rafal@clevertech.biz>
2017-09-18 13:59:09 +0200

    refactor: make BookShelf a Stateless Functional Component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,34 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
-class BookShelf extends Component {
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    books: PropTypes.array.isRequired,
-    onUpdateBookShelf: PropTypes.func.isRequired
-  }
+const BookShelf = ({title, books, onUpdateBookShelf}) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{title}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {books.map(book => (
+          <li key={book.id}>
+            <Book 
+              book={book} 
+              onUpdateBookShelf={(shelf) => onUpdateBookShelf(book, shelf)}
+            />
+          </li>
+        ))} 
+      </ol>
+    </div>
+  </div>
+);
 
-  render() {
-    const {title, books} = this.props;
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            {books.map(book => (
-              <li key={book.id}>
-                <Book 
-                  book={book} 
-                  onUpdateBookShelf={(shelf) => this.props.onUpdateBookShelf(book, shelf)}
-                />
-              </li>
-            ))} 
-          </ol>
-        </div>
-      </div>
-    )
-  }
-}
+BookShelf.propTypes = {
+  title: PropTypes.string.isRequired,
+  books: PropTypes.array.isRequired,
+  onUpdateBookShelf: PropTypes.func.isRequired
+};
 
-export default BookShelf
+export default BookShelf;
